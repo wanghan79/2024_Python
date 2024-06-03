e2a581a7a00a47a5d738e6dbe2aae6bb2140f126
Wjq <1377146983@qq.com>
2024-06-03 20:34:45 +0800

    第四次作业:迭代器的使用，避免了一次性生成和存储所有数据样本，从而节省了内存资源

--- 2022013524王佳琦/assignment_04.py
+++ 2022013524王佳琦/assignment_04.py
@@ -1,25 +1,66 @@
 import random
-
-def random_generator(min_value, max_value, num):
-    """
-    生成器函数，用于生成一系列在指定范围内的随机整数。
-
-    生成器行为:
-    每次迭代此生成器时，将返回一个新的随机整数，直到返回了指定数量的随机数为止。
+import string
 
 
-    """
-    for _ in range(num):
-        yield random.randint(min_value, max_value)
+class DataGenerator:
+    def __init__(self, data_struct):
+        self.data_struct = data_struct
+        self.subjects = ['Mathematics', 'Physics', 'Chemistry', 'Biology', 'Computer Science']
 
-# 获取用户输入
-min_value = int(input("请输入随机数的最小值："))
-max_value = int(input("请输入随机数的最大值："))
-num = int(input("请输入要生成的随机数的数量："))
+    def _gen_string(self, datarange, length):
+        for _ in range(length):
+            yield random.choice(datarange)
 
-# 创建随机数生成器对象
-random_nums = random_generator(min_value, max_value, num)
+    def _gen_int(self, datarange):
+        start, end = datarange
+        while True:
+            yield random.randint(start, end)
 
-# 通过迭代生成器获取并打印随机数
-for num in random_nums:
-    print(num)
+    def _gen_float(self, datarange):
+        start, end = datarange
+        while True:
+            yield round(random.uniform(start, end), 2)
+
+    def generate_sample(self, data_type, data_options):
+        if data_type == 'string':
+            datarange = data_options.get('datarange', string.ascii_uppercase)
+            len_range = data_options.get('len', (5, 10))
+            str_len = random.randint(*len_range)
+            return ''.join(self._gen_string(datarange, str_len))
+        elif data_type == 'int':
+            datarange = data_options.get('datarange', (1, 100))
+            return next(self._gen_int(datarange))
+        elif data_type == 'float' or data_type == 'double':
+            datarange = data_options.get('datarange', (0.0, 100.0))
+            return next(self._gen_float(datarange))
+        elif data_type == 'list':
+            datarange = self.subjects
+            return random.sample(datarange, min(len(datarange), random.randint(1, len(datarange))))
+        elif data_type == 'tuple':
+            datarange = tuple(self.subjects)
+            return tuple(random.sample(list(datarange), min(len(datarange), random.randint(1, len(datarange)))))
+
+    def struct_data_sampling(self, num_samples):
+        for _ in range(num_samples):
+            sample = {}
+            for data_type, data_options in self.data_struct.items():
+                sample[data_type] = self.generate_sample(data_type, data_options)
+            yield sample
+
+# 定义数据结构
+data_struct = {
+    'string': {'datarange': string.ascii_uppercase, 'len': (5, 10)},
+    'int': {'datarange': (1, 100)},
+    'float': {'datarange': (0.0, 100.0)},
+    'double': {'datarange': (0.0, 100.0)},
+    'list': {},
+    'tuple': {}
+}
+
+# 创建 DataGenerator 实例并生成样本
+generator = DataGenerator(data_struct)
+
+# 生成并打印样本
+num_samples = 5
+for sample in generator.struct_data_sampling(num_samples):
+    print(sample)
