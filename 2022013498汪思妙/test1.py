caa4982986c430eb2560dd363221f420fddea7f1
AbbiE <167288542+FragileAbbiE@users.noreply.github.com>
2024-06-06 16:04:51 +0800

    Create test1.py

---
+++ 2022013498汪思妙/test1.py
@@ -0,0 +1,47 @@
+import random
+import string
+
+def generate_random_data(data_struct):
+    sample = {}
+    for data_type, data_options in data_struct.items():
+        if data_type == 'string':
+            datarange = data_options.get('datarange', string.ascii_uppercase)
+            len_range = data_options.get('len', (5, 10))
+            str_len = random.randint(len_range[0], len_range[1])
+            sample[data_type] = ''.join(random.choices(datarange, k=str_len))
+        elif data_type == 'int':
+            datarange = data_options.get('datarange', (1, 100))
+            sample[data_type] = random.randint(datarange[0], datarange[1])
+        elif data_type == 'float' or data_type == 'double':
+            datarange = data_options.get('datarange', (0.0, 100.0))
+            sample[data_type] = round(random.uniform(datarange[0], datarange[1]), 2)
+        elif data_type == 'list':
+            datarange = ['Mathematics', 'Physics', 'Chemistry', 'Biology', 'Computer Science']
+            sample[data_type] = random.sample(datarange, min(len(datarange), random.randint(1, len(datarange))))
+        elif data_type == 'tuple':
+            datarange = tuple(datarange)
+            sample[data_type] = tuple(random.sample(list(datarange), min(len(datarange), random.randint(1, len(datarange)))))
+    return sample
+
+def generate_samples(num_samples, data_struct):
+    results = []
+    for _ in range(num_samples):
+        sample = generate_random_data(data_struct)
+        results.append(sample)
+    return results
+
+# 定义数据结构和样本数量
+num_samples = 5  # 这里设定生成5个样本
+data_struct = {
+    'string': {'datarange': string.ascii_uppercase, 'len': (5, 10)},
+    'int': {'datarange': (1, 100)},
+    'float': {'datarange': (0.0, 100.0)},
+    'double': {'datarange': (0.0, 100.0)},
+    'list': {},
+    'tuple': {}
+}
+
+samples = generate_samples(num_samples, data_struct)
+
+for sample in samples:
+    print(sample)
