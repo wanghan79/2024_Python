bea858a1cb2bb1699b4c328afbc1662538b6785e
Diligentcapybara <139529801+Diligentcapybara@users.noreply.github.com>
2024-06-13 16:59:23 +0800

    Update 第四次作业.py

--- 2022012104韩绪/第四次作业.py
+++ 2022012104韩绪/第四次作业.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 def decorator(aClass):
     class DecoratorClass():
@@ -8,8 +9,8 @@ def decorator(aClass):
         def dataExtraction(self, example, num):
             return self.aClass.dataExtraction(example, num)
 
-        def data_iterator(self, example, n):
-            for i in range(n):
+        def data_yield(self, example, num):
+            for i in range(num):
                 yield self.dataExtraction(example, num=1)
 
     return DecoratorClass
@@ -23,6 +24,64 @@ class DataProcessor:
             'id': {'1', '2', '4'}
         }
 
+        self.example_template = {
+            "datatype": tuple,
+            "subs": {
+                "name": {
+                    "datatype": str,
+                    "datarange": self.data_range['name'],
+                    "len": 1  # specifying length 1 for single random choice
+                },
+                "age": {
+                    "datatype": str,
+                    "datarange": self.data_range['age'],
+                    "len": 1
+                },
+                "id": {
+                    "datatype": str,
+                    "datarange": self.data_range['id'],
+                    "len": 1
+                }
+            }
+        }
+
+    def generate_data(self, template):
+        datatype = template.get("datatype")
+        subs = template.get("subs", {})
+
+        result = {
+            tuple: (),
+            list: [],
+            dict: {},
+            set: set()
+        }[datatype]
+
+        for key, value in subs.items():
+            data = self.generate_item(value)
+            if datatype == tuple:
+                result += (data,)
+            elif datatype == list:
+                result.append(data)
+            elif datatype == dict:
+                result[key] = data
+            elif datatype == set:
+                result.add(data)
+
+        return result
+
+    def generate_item(self, value):
+        if "datatype" in value and "datarange" in value:
+            if value["datatype"] == int:
+                return random.randint(*value["datarange"])
+            elif value["datatype"] == float:
+                return random.uniform(*value["datarange"])
+            elif value["datatype"] == bool:
+                return random.choice([True, False])
+            elif value["datatype"] == str:
+                datarange_list = list(value["datarange"])  # Convert set to list
+                return ''.join(random.choices(datarange_list, k=value.get("len", 1)))
+        return self.generate_data(value)
+
     def dataExtraction(self, example, num):
         result = set()
         for _ in range(num):
@@ -34,12 +93,12 @@ class DataProcessor:
             result.add(tuple(temp))
         return result
 
-    def data_iterator(self, example, n):
-        return self.aClass.data_iterator(example, n)
+    def data_yield(self, example, num):
+        return self.aClass.data_yield(example, num)
 
 if __name__ == '__main__':
-    dataProcessor = DataProcessor()
+    processor = DataProcessor()
 
-    example = ('id', 'name', 'age')
-    for data in dataProcessor.data_iterator(example, n=5):
-        print(data)
+    example = ('name', 'age', 'id')
+    for data in processor.data_yield(example, num=3):
+        print(str(data))
