6e0505949b82b1c1ac4c01e4023b111b90276aa5
Diligentcapybara <139529801+Diligentcapybara@users.noreply.github.com>
2024-06-13 19:27:06 +0800

    Update 第二次作业.py

--- 2022012104韩绪/第二次作业.py
+++ 2022012104韩绪/第二次作业.py
@@ -1,35 +1,10 @@
 import random
-import string
+from example_template import example_template
 
-class DataProcessor:
-    def __init__(self):
-        self.data_range = {
-            'name': {'John', 'Smith','Bob'},
-            'age': {'20', '18', '22'},
-            'id': {'1', '2', '4'}
-        }
-
-        self.example_template = {
-            "datatype": tuple,
-            "subs": {
-                "name": {
-                    "datatype": str,
-                    "datarange": self.data_range['name'],
-                    "len": 1  # specifying length 1 for single random choice
-                },
-                "age": {
-                    "datatype": str,
-                    "datarange": self.data_range['age'],
-                    "len": 1
-                },
-                "id": {
-                    "datatype": str,
-                    "datarange": self.data_range['id'],
-                    "len": 1
-                }
-            }
-        }
-
+class Data_sampler():
+    def __init__(self, example_template):
+        self.example_template = example_template
+    
     def generate_data(self, template):
         datatype = template.get("datatype")
         subs = template.get("subs", {})
@@ -57,30 +32,26 @@ class DataProcessor:
     def generate_item(self, value):
         if "datatype" in value and "datarange" in value:
             if value["datatype"] == int:
-                return random.randint(*value["datarange"])
+                return random.randint(value["datarange"][0], value["datarange"][1])
             elif value["datatype"] == float:
-                return random.uniform(*value["datarange"])
-            elif value["datatype"] == bool:
-                return random.choice([True, False])
+                return random.uniform(value["datarange"][0], value["datarange"][1])
             elif value["datatype"] == str:
-                datarange_list = list(value["datarange"])  # Convert set to list
-                return ''.join(random.choices(datarange_list, k=value.get("len", 1)))
+                return random.SystemRandom().choice(value['datarange'])
         return self.generate_data(value)
 
-    def dataExtraction(self, example, num):
-        result = set()
+
+    def dataExtraction(self, num):
+        result = []
         for _ in range(num):
             extracted_data = self.generate_data(self.example_template)
-            result.add(extracted_data)
+            result.append(extracted_data)
         return result
 
 if __name__ == '__main__':
-    processor = DataProcessor()
 
-    example = ('name', 'age', 'id')  # Define example here
-    extracted_data = processor.dataExtraction(example, num=3)
+    data_sampler = Data_sampler(example_template)
+    extracted_data = data_sampler.dataExtraction(num=3)
 
     print("Generated random data:")
     for data in extracted_data:
         print(data)
-
