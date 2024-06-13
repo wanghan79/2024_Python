3df43af0e6037c9f0a15d74e0696f9bbea6920c3
Diligentcapybara <139529801+Diligentcapybara@users.noreply.github.com>
2024-06-13 16:52:05 +0800

    Update 第四次作业.py

--- 2022012104韩绪/第四次作业.py
+++ 2022012104韩绪/第四次作业.py
@@ -6,7 +6,7 @@ def decorator(aClass):
             self.aClass = aClass()
 
         def dataExtraction(self, example, num):
-            print(self.aClass.dataExtraction(example, num))
+            return self.aClass.dataExtraction(example, num)
 
         def data_iterator(self, example, n):
             for i in range(n):
@@ -14,30 +14,15 @@ def decorator(aClass):
 
     return DecoratorClass
 
-
 @decorator
 class DataProcessor:
     def __init__(self):
         self.data_range = {
-            'name': {'John', 'Smith','Bob'},
+            'name': {'John', 'Smith', 'Bob'},
             'age': {'20', '18', '22'},
             'id': {'1', '2', '4'}
         }
-    """
-    def dataSampling(self, datatype, datarange, num, strlen=10):
-        result = set()
-        for index in range(num):
-            if datatype == int:
-                item = random.randint(datarange[0], datarange[1])
-                result.add(item)
-            elif datatype == float:
-                item = random.uniform(datarange[0], datarange[1])
-                result.add(item)
-            elif datatype == str:
-                item = ''.join(random.choices(datarange, k=strlen))
-                result.add(item)
-        return result
-    """
+
     def dataExtraction(self, example, num):
         result = set()
         for _ in range(num):
@@ -50,30 +35,11 @@ class DataProcessor:
         return result
 
     def data_iterator(self, example, n):
-        pass
-
-def yield_test(n:int):
-    i = 0
-    while i <= n:
-        i += 1
-        tmp = yield i
-        if tmp is not None:
-            i = tmp
-
+        return self.aClass.data_iterator(example, n)
 
 if __name__ == '__main__':
-
     dataProcessor = DataProcessor()
-    # dataProcessor.dataExtraction(example=('id', 'name', 'age'), num=10)
-    data_it = dataProcessor.data_iterator(example=('id', 'name', 'age'), n=50)
-    next(data_it)
-    next(data_it)
-    next(data_it)
-'''
-processor = DataProcessor()
- example = ('name', 'age', 'id')
- extracted_data = processor.dataExtraction(example, num=3)
- print("随机抽取的数据集：")
- for data in extracted_data:
-     print(data)
- '''
+
+    example = ('id', 'name', 'age')
+    for data in dataProcessor.data_iterator(example, n=5):
+        print(data)
